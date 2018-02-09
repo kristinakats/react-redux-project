847d2cf382c9d608cceed4fee8ff2e18d96fa856
Vasilis Georgopoulos <vgeorgopoulosit@gmail.com>
2018-02-09 23:32:01 +0200

    Fixed (PLEASE USE GIT)

---
+++ config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+  googleClientID:'906590604016-4894d85ahb35sf261kmh2li42b8215s5.apps.googleusercontent.com',
+  googleClientSecret: '1hflpwoLLHFvi_fK0OQWbAeF',
+mongoURI:'mongodb://kristina:password@ds247047.mlab.com:47047/emailly-dev',
+  cookieKey: 'djjjjjjjjjjjjjddndncnxdkjfsir'
+};
