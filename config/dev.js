1f13bd1b866613850565f11ea8de4c2b7d02ac49
kristin <katsarixristina90@gmail.com>
2018-03-12 22:17:16 +0200

    Added billing and client side app!

--- config/dev.js
+++ config/dev.js
@@ -1,6 +1,9 @@
 module.exports = {
   googleClientID:'906590604016-4894d85ahb35sf261kmh2li42b8215s5.apps.googleusercontent.com',
   googleClientSecret: '1hflpwoLLHFvi_fK0OQWbAeF',
-mongoURI:'mongodb://kristina:password@ds247047.mlab.com:47047/emailly-dev',
-  cookieKey: 'djjjjjjjjjjjjjddndncnxdkjfsir'
+  mongoURI:'mongodb://kristina:password@ds247047.mlab.com:47047/emailly-dev',
+  cookieKey: 'djjjjjjjjjjjjjddndncnxdkjfsir',
+  stripePublishableKey: 'pk_test_hKkIFwvDSDDOmaO8cFiqDC9l',
+  stripeSecretKey: 'sk_test_cXN90PrmVgnynHibLwi4ZCyh'
+
 };
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cookieSession = require ("cookie-session");
 const passport = require ("passport");
+const bodyParser = require('body-parser');
 const keys = require("./config/keys");
 require("./models/User");
 require("./services/passport");
@@ -14,7 +15,7 @@ useMongoClient: true
 });
 
 const app = express();
-
+app.use(bodyParser.json());
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
@@ -29,5 +30,19 @@ app.use(passport.session());
 const PORT = process.env.PORT || 5000;
 
 require("./routes/authRoutes")(app);
+require("./routes/billingRoutes") (app);
+
+if (process.env.NODE_ENV === 'production'){
+  //Express will serve up production assets
+  //like our main.js file or main.css file
+  app.user(express.static('client/build'));
+
+  //Express will serve up the index.html file
+  // if it doesn't recognize the route
+  const path =require('path');
+  app.get('*', (req,res) =>{
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  });
+}
 
 app.listen(PORT);
